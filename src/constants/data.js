46efcffca5d1d65718aa57881ac30bbf6fcad61b
AbishekBista <abishekbista84@gmail.com>
2023-06-14 22:32:27 +0545

    textual changes

--- src/constants/data.js
+++ src/constants/data.js
@@ -60,22 +60,26 @@ const awards = [
   {
     imgUrl: images.award02,
     title: "Bib Gourmond",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
+    subtitle:
+      "Culinary Brilliance Recognized: A Distinguished Accolade Earned.",
   },
   {
     imgUrl: images.award01,
     title: "Rising Star",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
+    subtitle:
+      "Shining Bright: Rising Star Acknowledgment for Culinary Excellence.",
   },
   {
     imgUrl: images.award05,
     title: "AA Hospitality",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
+    subtitle:
+      "Excellence Acknowledged: Prestigious Award for Exceptional Service.",
   },
   {
     imgUrl: images.award03,
     title: "Outstanding Chef",
-    subtitle: "Lorem ipsum dolor sit amet, consectetur.",
+    subtitle:
+      "Master of Culinary Craft: Acclaimed Outstanding Chef Award Achieved.",
   },
 ];
 
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => (
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <p className="p__opensans">Embrace the Essence of Japanese Gastronomy at Gericht, where we harmoniously blend tradition and innovation to deliver unparalleled culinary experiences.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
 
@@ -24,7 +24,7 @@ const AboutUs = () => (
     <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
-        <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+        <p className="p__opensans">Since 1981, Gericht has been a pioneer in the realm of Japanese fine dining, preserving centuries-old traditions while embracing modern culinary evolution.</p>
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
